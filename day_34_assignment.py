73260a77a072bdd5ec614718e7f0edf11b960325
Carissa DeRanek <cderanek@RCSs-MacBook-Pro.local>
2019-12-10 14:15:58 -0800

    added guiding questions

---
+++ day_34_assignment.py
@@ -0,0 +1,17 @@
+''' 
+What branch do you start on? How do you know?
+
+Create a new branch called "feature".
+
+Switch to the feature branch and work on this assignment. Everytime you make 
+a meaningful change, commit that change with a useful commit message.
+
+Now try to add, commit, and push your work. You should get an error message.
+Copy/paste the error message (or specific pieces you don't quite understand)
+into a search engine. Did you find any helpful explanations for the error 
+message? (I recommend looking for highly upvoted responses on StackOverflow)
+What does the error message mean? What should you do to push your work to GitHub?
+
+After committing and pushing your work, log back into GitHub on your browser. Do
+you see your commits? Can you see the history of your edits?
+'''
